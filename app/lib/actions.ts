3c56d46ead7fe93e1129e37cfac04ec2e2309c4b
sebascarp9 <sebascarp9@gmail.com>
2024-08-27 12:10:28 +0000

    zod implemented and schema form created

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,5 +1,13 @@
 'use server';
+import z from 'zod'
 
+const formSchema = z.object({
+  id: z.string(),
+  customerId: z.string(),
+  amount: z.coerce.number(),
+  status: z.enum(['pending', 'paid']),
+  date: z.string(),
+})
 export async function createInvoice(formData: FormData) {
     const rawFormData = {
       customerId: formData.get('customerId'),
@@ -7,5 +15,6 @@ export async function createInvoice(formData: FormData) {
       status: formData.get('status'),
     };
     // Test it out:
+    console.log(typeof rawFormData.amount);
     console.log(rawFormData);
   }
