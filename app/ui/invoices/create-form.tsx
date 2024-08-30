21c38bb11f2fdd0b7922d0252368c9b810ed4559
sebastiangaleano.dev <sebastiangaleano.dev@gmail.com>
2024-08-30 12:38:32 +0000

    Improving accessibility edit form file configurated

--- app/ui/invoices/create-form.tsx
+++ app/ui/invoices/create-form.tsx
@@ -88,7 +88,7 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
             Set the invoice status
           </legend>
           <div className="rounded-md border border-gray-200 bg-white px-[14px] py-3">
-            <div className="flex gap-4">
+            <div className="flex gap-4" aria-describedby="status-error">
               <div className="flex items-center">
                 <input
                   id="pending"
@@ -96,7 +96,7 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
                   type="radio"
                   value="pending"
                   className="h-4 w-4 cursor-pointer border-gray-300 bg-gray-100 text-gray-600 focus:ring-2"
-                  
+
                 />
                 <label
                   htmlFor="pending"
@@ -121,6 +121,14 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
                 </label>
               </div>
             </div>
+          </div>
+          <div id="status-error" aria-live="polite" aria-atomic="true">
+            {state.errors?.status &&
+              state.errors.status.map((error: string) => (
+                <p className="mt-2 text-sm text-red-500" key={error}>
+                  {error}
+                </p>
+              ))}
           </div>
         </fieldset>
       </div>
